3ea19789f7dfb8adf2a187d835bc359e937b6619
HarshPadishalwar29 <harsh.padishalwar123@gmail.com>
2025-03-25 11:32:16 +0530

    Create T1.py

---
+++ T1.py
@@ -0,0 +1,8 @@
+# T1.py
+def greet(name):
+    """Function to greet the user"""
+    return f"Hello, {name}! Welcome to Python programming."
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    print(greet(user_name))
