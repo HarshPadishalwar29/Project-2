daaa46d3af02072bd365cef917a98a2419d64d4a
HarshPadishalwar29 <harsh.padishalwar123@gmail.com>
2025-03-25 11:37:45 +0530

    Update T1.py

--- T1.py
+++ T1.py
@@ -4,5 +4,5 @@ def greet(name):
     return f"Hello, {name}! Welcome to Python programming."
 
 if __name__ == "__main__":
-    user_name = input("Enter your name: ")
+    user_name = input("Can you please tell your name: ")
     print(greet(user_name))
